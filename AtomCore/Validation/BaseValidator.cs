ac68b769f67c6280f7cc1ac57ff93bc7f599da18
ikhan <qadirovayxan@gmail.com>
2024-12-30 00:56:44 +0400

    Add Lab project, FluentValidation, and BaseValidator

---
+++ AtomCore/Validation/BaseValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AtomCore.Validation;
+
+public class BaseValidator<T> : AbstractValidator<T>
+{
+    protected bool BePhoneNumber(string text)
+    {
+        var isMatch = Regex.IsMatch(text, @"^\+[1-9]\d{1,14}$");
+        return isMatch;
+    }
+
+    protected bool ContainAtLeastOneNumberCharacter(string text)
+    {
+        foreach (var ch in text)
+            if (ch >= 48 && ch <= 57)
+                return true;
+
+        return false;
+    }
+}
---
+++ Lab/Program.cs
@@ -0,0 +1,6 @@
+﻿string text = "12A";
+
+foreach(char ch in text)
+{
+    Console.WriteLine((int)ch);
+}
