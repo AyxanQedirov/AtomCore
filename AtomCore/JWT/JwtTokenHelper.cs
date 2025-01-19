380f965a701318887bcb63b4f5c34233e75f8172
ikhan <qadirovayxan@gmail.com>
2025-01-20 01:15:42 +0400

    added new method to JwtTokenHelper

--- AtomCore/JWT/JwtTokenHelper.cs
+++ AtomCore/JWT/JwtTokenHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
@@ -20,18 +21,17 @@ public class JwtTokenHelper
 
     public string CreateToken(params Claim[] claims)
     {
-        string key = _configuration["Jwt:Key"] ?? throw new ArgumentNullException("Key was not settd. appsettings.json > Jwt > Key");
+        string secretKey = _configuration["Jwt:Key"] ?? throw new ArgumentNullException("Key was not settd. appsettings.json > Jwt > Key");
         string expireMinute = _configuration["Jwt:ExpiredDateAsMinute"] ?? throw new ArgumentNullException("ExpiredDateAsMinute was not settd. appsettings.json > Jwt > ExpiredDateAsMinute");
         string audience = _configuration["Jwt:Audience"] ?? throw new ArgumentNullException("Audience was not settd. appsettings.json > Jwt > Audience");
         string issuer = _configuration["Jwt:Issuer"] ?? throw new ArgumentNullException("Issuer was not settd. appsettings.json > Jwt > Issuer");
 
 
-        return CreateToken(key,int.Parse(expireMinute),audience,issuer,claims);
+        return CreateToken(secretKey, int.Parse(expireMinute), audience, issuer, claims);
     }
-
-    public string CreateToken(string key, int expireAdditionAsMinute, string audience, string issuer, params Claim[] claims)
+    public string CreateToken(string secretKey, int expireAdditionAsMinute, string audience, string issuer, params Claim[] claims)
     {
-        SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(key));
+        SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(secretKey));
         SigningCredentials signingCredentials = new(securityKey, SecurityAlgorithms.HmacSha512);
         DateTime expirationDate = DateTime.Now.AddMinutes(expireAdditionAsMinute);
 
@@ -50,19 +50,21 @@ public class JwtTokenHelper
     }
     public bool ValidateToken(string rawToken)
     {
-        if (rawToken is null)
-            throw new ArgumentNullException("Token which you give is null");
-
-        string key = _configuration["Jwt:Key"] ?? throw new ArgumentNullException("Key was not settd. appsettings.json > Jwt > Key");
+        string secretKey = _configuration["Jwt:SecretKey"] ?? throw new ArgumentNullException("SecretKey was not settd. appsettings.json > Jwt > Key");
         string audience = _configuration["Jwt:Audience"] ?? throw new ArgumentNullException("Audience was not settd. appsettings.json > Jwt > Audience");
         string issuer = _configuration["Jwt:Issuer"] ?? throw new ArgumentNullException("Issuer was not settd. appsettings.json > Jwt > Issuer");
 
-
+        return ValidateToken(rawToken, secretKey, audience, issuer);
+    }
+    public bool ValidateToken(string rawToken, string secretKey, string audience, string issuer)
+    {
+        if (rawToken is null)
+            throw new ArgumentNullException("Token which you give is null");
 
         string token = NormalizeRawToken(rawToken);
 
         JwtSecurityTokenHandler tokenHandler = new();
-        SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(key));
+        SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(secretKey));
 
         try
         {
@@ -84,7 +86,6 @@ public class JwtTokenHelper
         {
             return false;
         }
-
     }
     public string NormalizeRawToken(string rawToken)
     {
