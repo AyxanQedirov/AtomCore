99a6352d91537841ac0f2a7c7aca3726af43f005
AyxanQadirovMac <qadirovayxan@gmail.com>
2025-04-05 18:30:19 +0400

    added new validator for AZE phone number

--- AtomCore/Validation/BaseValidator.cs
+++ AtomCore/Validation/BaseValidator.cs
@@ -13,7 +13,13 @@ public class BaseValidator<T> : AbstractValidator<T>
     protected bool BePhoneNumber(string text)
     {
         var isMatch = Regex.IsMatch(text, @"^\+(\d{1,4}) (\d{4,14})$");
-            
+
+        return isMatch;
+    }
+
+    protected bool BeAZEPhoneNumber(string text)
+    {
+        var isMatch = Regex.IsMatch(text, @"^(\d{2})\-(\d{7})$");
         return isMatch;
     }
 
@@ -30,8 +36,8 @@ public class BaseValidator<T> : AbstractValidator<T>
     {
         //65 => A
         //90 => Z
-        foreach(var ch in text)
-            if(ch>=65 && ch<=90)
+        foreach (var ch in text)
+            if (ch >= 65 && ch <= 90)
                 return true;
 
         return false;
