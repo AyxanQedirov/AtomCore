9dbefc02a246a6ff80f4af5621d8404f76b35199
AyxanQadirovMac <qadirovayxan@gmail.com>
2025-06-14 14:47:09 +0400

    new version

---
+++ AtomCore/OpenAPI/OpenApiOptionsExtensions.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
+namespace AtomCore.OpenAPI;
+
+public static class OpenApiOptionsExtensions
+{
+    public static OpenApiOptions AddJwtAuthentication(this OpenApiOptions options)
+    {
+        options.AddDocumentTransformer((document, _, _) =>
+        {
+            document.Components ??= new OpenApiComponents();
+
+            document.Components.SecuritySchemes["Bearer"] = new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                Description = "Enter your JWT token (without �Bearer � prefix)"
+            };
+
+            document.SecurityRequirements.Add(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id   = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+
+            return  Task.CompletedTask;
+        });
+
+        return options;
+    }
+}
